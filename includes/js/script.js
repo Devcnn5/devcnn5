27bf583543556414436b48150ef81219286c8cff
devcnn5 <devcnn5@gmail.com>
2017-03-10 13:46:20 +0530

    Initial commit

---
+++ includes/js/script.js
@@ -0,0 +1,16 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$('document').ready(function()
+{
+  $('#myCarousel').carousel({  interval:2000});
+
+});
